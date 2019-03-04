8049bf56f9caf8e7932b5a399f7b1d7f42a47143
Giovanni Panasiti <giova.panasiti@gmail.com>
2019-03-04 17:14:51 +0100

    WIP function

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// Split a string in substrings each N characters
+func SplitSubstringN(s string, n int) []string {
+	sub := ""
+	subs := []string{}
+
+	runes := bytes.Runes([]byte(s))
+	l := len(runes)
+	for i, r := range runes {
+		sub = sub + string(r)
+		if (i+1)%n == 0 {
+			subs = append(subs, sub)
+			sub = ""
+		} else if (i + 1) == l {
+			subs = append(subs, sub)
+		}
+	}
+
+	return subs
+}
+
+// remove duplicates from a int32 array
+func RemoveDuplicatesInt32(a []int32) []int32 {
+	r := []int32{}
+	seen := map[int32]int32{}
+	for _, val := range a {
+		if _, ok := seen[val]; !ok {
+			r = append(r, val)
+			seen[val] = val
+		}
+	}
+	return r
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
